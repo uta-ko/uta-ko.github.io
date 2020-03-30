d0e5a5123ffd8f4724f34a460ffa671a5fec019c
uta-ko <52099782+uta-ko@users.noreply.github.com>
2020-03-30 20:58:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
-const medias = {
-
-  audio: false,
-
-  video: {
-
-    facingMode: "user"
-
-  }
-
-};
-
-const video = document.getElementById("video"); const promise = navigator.mediaDevices.getUserMedia(medias);
-
-
+const medias = {audio: false,video: {width: 1280, height: 720}};
+const video = document.getElementById("video");
+const promise = navigator.mediaDevices.getUserMedia(medias);
 
 promise.then(successCallback)
-
        .catch(errorCallback);
 
-
-
-
-
 function successCallback(stream) {
-
   video.srcObject = stream;
-
 };
 
-
-
 function errorCallback(err) {
-
   alert(err);
-
 };
 
