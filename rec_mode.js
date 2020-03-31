b4033ee5f6990f92d843f14a2b80d3a8308fad2c
uta-ko <52099782+uta-ko@users.noreply.github.com>
2020-03-31 20:32:58 +0900

    Update rec_mode.js

--- rec_mode.js
+++ rec_mode.js
@@ -1,3 +1,10 @@
+var recorder;
+var canvas = document.getElementById("video");
+var cnt = 0;
+var frame_no = 0;
+var imgsrc = [];
+var ctx2 = canvas.getContext("2d");
+
 const medias = {
   audio: false,
   video: {facingMode: {exact : 'environment'}}
@@ -15,3 +22,30 @@ function successCallback(stream) {
 function errorCallback(err) {
   alert(err);
 };
+
+//recボタンを押したとき
+function startVideo(){
+  var stream = canvas.captureStream();
+	    //ストリームからMediaRecorderを生成
+	    recorder = new MediaRecorder(stream,{mimeType:'video/webm;codecs=vp8'});
+	    //ダウンロード用のリンクを準備
+	    var anchor = document.getElementById('downloadlink');
+    	//録画終了時に動画ファイルのダウンロードリンクを生成する処理
+    	recorder.ondataavailable = function(e) {
+	  	var videoBlob = new Blob([e.data], { type: e.data.type });
+	  	blobUrl = window.URL.createObjectURL(videoBlob);
+	    anchor.download = 'movie.webm';
+	    anchor.href = blobUrl;
+      anchor.style.display = 'block';
+    }
+      //録画開始
+      recorder.start();
+      timer1 = setInterval(getImage,33);
+};
+
+function stopVideo(){
+  clearInterval(timer1);
+      recorder.stop();
+};
+
+
