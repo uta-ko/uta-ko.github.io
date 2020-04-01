3988ee695f90ad966339e535c9769a5ecfa3d9a1
uta-ko <52099782+uta-ko@users.noreply.github.com>
2020-04-01 00:44:28 +0000

    Update rec_mode.js

--- rec_mode.js
+++ rec_mode.js
@@ -1,3 +1,4 @@
+
 var recorder;
 var canvas = document.getElementById("picture");
 var cnt = 0;
@@ -5,27 +6,46 @@ var frame_no = 0;
 var imgsrc = [];
 var ctx2 = canvas.getContext("2d");
 
-const medias = {
-  audio: false,
-  video: {facingMode: {exact : 'environment'}}
-};
-const video = document.getElementById("camera");
-const promise = navigator.mediaDevices.getUserMedia(medias);
+window.onload = () => {
+    const video  = document.querySelector("#camera");
+  
+    /** カメラ設定 */
+    const constraints = {
+      audio: false,
+      video: {
+        width: 300,
+        height: 170,
+        facingMode: "user"   // フロントカメラを利用する
+        // facingMode: { exact: "environment" }  // リアカメラを利用する場合
+      }
+    };
+  
+    /*カメラを<video>と同期*/
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then( (stream) => {
+      video.srcObject = stream;
+      video.onloadedmetadata = (e) => {
+        video.play();
+      };
+    })
+    .catch( (err) => {
+      console.log(err.name + ": " + err.message);
+    });
+  
+    let timer1;
+    let getImage = function(){
+        ctx2.drawImage(video,0,0,canvas.width, canvas.height);
+        imgsrc.push(ctx2);
+        console.log("frameno");
+        cnt ++
+        
+        
+    };
+  
+    // 開始ボタン
+    document.getElementById("startbtn").addEventListener("click",() =>{
 
-promise.then(successCallback)
-       .catch(errorCallback);
-
-function successCallback(stream) {
-  video.srcObject = stream;
-};
-
-function errorCallback(err) {
-  alert(err);
-};
-
-//recボタンを押したとき
-function startVideo(){
-  var stream = canvas.captureStream();
+      var stream = canvas.captureStream();
 	    //ストリームからMediaRecorderを生成
 	    recorder = new MediaRecorder(stream,{mimeType:'video/webm;codecs=vp8'});
 	    //ダウンロード用のリンクを準備
@@ -41,11 +61,15 @@ function startVideo(){
       //録画開始
       recorder.start();
       timer1 = setInterval(getImage,33);
-};
+      
 
-function stopVideo(){
-  clearInterval(timer1);
+    })
+    // 停止ボタン
+    document.getElementById("endbtn").addEventListener("click",() =>{
+      clearInterval(timer1);
       recorder.stop();
-};
-
-
+      
+      
+    })
+  
+  };
