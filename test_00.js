5c40db6c5ed4b7bb63c4160a0eb8d2050dcd2888
uta-ko <52099782+uta-ko@users.noreply.github.com>
2020-04-08 16:18:09 +0900

    Update test_00.js

--- test_00.js
+++ test_00.js
@@ -42,8 +42,9 @@ function predict(){
         var tensor = tf.image.resizeNearestNeighbor(fp,[16, 16]).toFloat();
         var offset = tf.scalar(255);
         var tensor_image = tensor.div(offset).expandDims();
-        console.log(model.predict(tensor_image));
-		return model.predict(tensor_image);
+        //console.log(model.predict(tensor_image));
+        let prediction = await model.predict(tensor_image);
+        return prediction;
        }
        
        run();
