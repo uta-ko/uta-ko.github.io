9631b4a1ebbbbbff1802c08854646f87ee390c9e
uta-ko <52099782+uta-ko@users.noreply.github.com>
2020-04-09 14:14:31 +0900

    Update test_00.js

--- test_00.js
+++ test_00.js
@@ -51,7 +51,7 @@ function predict(){
         var tensor_image = tensor.div(offset).expandDims();
         array.push(tensor_image)
         let prediction = await model.predict(array);
-        let results = Array.from(prediction.print());   
+        let results = Array.from(prediction.data([0]));   
 
         prediction.print();
         console.log(results);
