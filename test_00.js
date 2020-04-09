a18cc87477ca77701dc7b8d7380a3a522d66a421
uta-ko <52099782+uta-ko@users.noreply.github.com>
2020-04-09 15:02:35 +0900

    Update test_00.js

--- test_00.js
+++ test_00.js
@@ -51,18 +51,20 @@ function predict(){
         var tensor_image = tensor.div(offset).expandDims();
         array.push(tensor_image)
         let prediction = await model.predict(array).data();
-        let results = Array.from(prediction).map(function(p,i){
-            return {
-                probability: p,
-                className: CLASSES[i]
-            };
-            }).sort(function(a,b){
-                return b.probability-a.probability;
-            }).slice(0,5);
-         
-            results.forEach(function(p){
-                console.log(p.className,p.probability.toFixed(6))
-            });  
+        let results = Array.from(prediction)
+                    .map(function(p,i){
+                        return {
+                        probability: p,
+                        className: CLASSES[i]
+                    };
+                    }).sort(function(a,b){
+                        return b.probability-a.probability;
+                    }).slice(0,5);
+                
+                    results.forEach(function(p){
+                        console.log(p.className,p.probability.toFixed(6));
+                        document.write(p.className,p.probability.toFixed(6));
+                    });  
         
        }
     run();
